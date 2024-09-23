30343a2d261f0b7d3bc10678e3bd07e74de722cd
aryajason <aryajasonr@gmail.com>
2024-09-23 11:40:47 +0700

    Update main.cpp

--- Program to determine the largest number/main.cpp
+++ Program to determine the largest number/main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() 
 {
- int a,b,c;
-cout << "input number 1: " ;
+ int a,b,c; // declare variable
+cout << "input number 1: " ; // input a,b,c number
 cin >> a ;
 
 cout << "input number 2: " ;
@@ -13,15 +13,16 @@ cin >> b ;
 cout << "input number 3: " ;
 cin >> c ;
 
-int largest = a;
+int largest = a; // determine the largest number
 
-if (b > largest ) {
+if (b > largest ) { // compared the number
     largest = b ;
 }
 
 if (c > largest ) {
     largest = c ;
 }
-cout << "The large number: " << largest << endl ;
+cout << "The large number: " << largest << endl ; // output the largest number
 
+return 0; // end the program
 }
